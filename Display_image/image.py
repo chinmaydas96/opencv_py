49130388b040156ab9aed6ba688ece9452ba676e
chinmaydas96 <chinmaydasbat@gmail.com>
2016-04-09 14:59:52 +0530

    Display the image

--- Display_image/image.py
+++ Display_image/image.py
@@ -1,4 +1,17 @@
+# importing all the required library
 import cv2
-image=cv2.imread('/home/user/image.jpg',cv2.IMREAD_COLOR)
+import numpy as np
+
+# load an color image in grayscale whose value is 0,for color it's 1.
+# load the image and show it. 
+
+image=cv2.imread('/home/bat/messi5.jpg',0)
 cv2.imshow(' window ',image)
-cv2.waitkey()
+r = 100.0 / image.shape[1]
+dim = (100, int(image.shape[0] * r))
+ 
+# perform the actual resizing of the image and show it
+resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+cv2.imshow("resized", resized)
+cv2.waitKey(0)
+cv2.destroyWindow()
